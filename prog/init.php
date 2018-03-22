1feb122aa46b21fc9532353d36fa753c36fc9e1e
MelBou <bouzon.melissa@gmail.com>
2018-03-22 13:40:42 +0100

    mdp enlevé

--- prog/init.php
+++ prog/init.php
@@ -1,6 +1,6 @@
 <?php
 try {
-  $bdd = new PDO('mysql:host=localhost;dbname=bdhs;charset=utf8', 'root', 'root');
+  $bdd = new PDO('mysql:host=localhost;dbname=bdhs;charset=utf8', 'root', '');
 }
 
 catch(Exception $e) {
